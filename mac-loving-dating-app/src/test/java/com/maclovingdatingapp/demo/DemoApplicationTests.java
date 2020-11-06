198a3ea3f39cdadf4d1ff9e1ac6f6ccd5f1323df
Andreas Pedersen <andreas1992@outlook.dk>
2020-11-06 19:50:02 +0100

    seccond first update

---
+++ mac-loving-dating-app/src/test/java/com/maclovingdatingapp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maclovingdatingapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
