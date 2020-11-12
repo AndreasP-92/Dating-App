8c57ed197f769c0a9355d153aa4278a7280f88cb
Jeffermus <70596979+Jeffermus@users.noreply.github.com>
2020-11-12 02:36:45 +0100

    j
    jeffry

---
+++ demo/src/main/java/com/example/demo/DatingappJeffreyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingappJeffreyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingappJeffreyApplication.class, args);
+	}
+
+}
