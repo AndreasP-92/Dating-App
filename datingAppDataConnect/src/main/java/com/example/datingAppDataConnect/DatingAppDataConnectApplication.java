b8c8d4656d0731b33b3ab44b14a3ca1a45d8781b
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2020-11-12 02:21:37 +0100

    database work

---
+++ datingAppDataConnect/src/main/java/com/example/datingAppDataConnect/DatingAppDataConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.datingAppDataConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingAppDataConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingAppDataConnectApplication.class, args);
+	}
+
+}
---
+++ datingAppDataConnect/src/test/java/com/example/datingAppDataConnect/DatingAppDataConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datingAppDataConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAppDataConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
